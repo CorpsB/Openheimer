3722497293cc38ad8c13920c0fc9c7cbea408ca1
Rudolf Tom <tom.rudolf@epitech.eu>
2024-03-24 18:20:59 +0100

    feat: add doc.php + fix interruptor

--- game/game.php
+++ game/game.php
@@ -3,7 +3,7 @@
 $modules = array(
     "1" => "../modules/button/button.php",
     "2" => "../modules/fillbar/fillb.php",
-    // "2" => "",
+    "3" => "../modules/interuptors/interruptor.php",
     // "4" => "",
     // "5" => "",
     // "6" => "",
@@ -21,6 +21,7 @@ $seed = $_GET["id"];
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -31,14 +32,15 @@ $seed = $_GET["id"];
     <script src="../js/game.js"></script>
     <title>Open-heimer</title>
 </head>
+
 <body>
     <div class="content">
         <div class="title">
             <h1><i class="fas fa-clock"></i></h1>
             <h1 id="clock">02:00</h1>
             <!-- <div> -->
-                <!-- <h1><i class="fas fa-xmark"></i><p>0/2</p></h1> -->
-                <!-- <p>0/2</p> -->
+            <!-- <h1><i class="fas fa-xmark"></i><p>0/2</p></h1> -->
+            <!-- <p>0/2</p> -->
             <!-- </div> -->
             <a href="../index.php" id="return_to_home"><i class="fas fa-home"></i></a>
         </div>
@@ -57,13 +59,18 @@ $seed = $_GET["id"];
     <!-- <script src="../js/popup.js"></script> -->
     <div class="picture_bombe">
         <div class="bombe">
-            <div class="one"><?php require_once($modules[substr($seed, 0, 1)]);?></div>
-            <div class="two"><?php require_once($modules[substr($seed, 1, 1)]);?></div>
-            <div class="three"><?php // require_once($modules[substr($seed, 2, 1)]);?></div>
-            <div class="four"><?php // require_once($modules[substr($seed, 3, 1)]);?></div>
-            <div class="five"><?php // require_once($modules[substr($seed, 4, 1)]);?></div>
-            <div class="six"><?php // require_once($modules[substr($seed, 5, 1)]);?></div>
+            <div class="one"><?php require_once($modules[substr($seed, 0, 1)]); ?></div>
+            <div class="two"><?php require_once($modules[substr($seed, 1, 1)]); ?></div>
+            <div class="three"><?php // require_once($modules[substr($seed, 2, 1)]);
+                                ?></div>
+            <div class="four"><?php // require_once($modules[substr($seed, 3, 1)]);
+                                ?></div>
+            <div class="five"><?php // require_once($modules[substr($seed, 4, 1)]);
+                                ?></div>
+            <div class="six"><?php // require_once($modules[substr($seed, 5, 1)]);
+                                ?></div>
         </div>
     </div>
 </body>
+
 </html>
--- modules/interuptors/interruptor.php
+++ modules/interuptors/interruptor.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Button Styling Example</title>
-    <link rel="stylesheet" href="i_style.css">
+    <link rel="stylesheet" href="../modules/interuptors/i_style.css">
 </head>
 
 <div class="button-container">
@@ -18,9 +19,10 @@
 <script>
     document.querySelector('.button-container').addEventListener('click', function(event) {
         if (event.target.className === 'btn') {
-          const buttonNumber = event.target.textContent.match(/\d+/)[0];
-          alert(`You clicked Button ${buttonNumber}`);
+            const buttonNumber = event.target.textContent.match(/\d+/)[0];
+            alert(`You clicked Button ${buttonNumber}`);
         }
-      });
-    </script>
+    });
+</script>
+
 </html>
