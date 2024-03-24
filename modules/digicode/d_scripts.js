cc7f540f1c8def770c5e87fe964499d38c3a3f6b
ncarabin <noe.carabin@epitech.eu>
2024-03-24 22:11:26 +0100

    Add : Digicode

---
+++ modules/digicode/d_scripts.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const buttons = document.querySelectorAll('.square');
+    let count = 0;
+    const correctButtons = [1, 3, 5, 7];
+    let pressedButtons = [];
+
+    buttons.forEach((button, index) => {
+        button.addEventListener('click', function () {
+            if (!button.classList.contains('clicked')) { 
+                button.style.backgroundColor = 'yellow';
+                button.classList.add('clicked');
+                count++;
+                pressedButtons.push(index);
+                
+                
+                if (count === 4) {
+                    if (pressedButtons.every(val => correctButtons.includes(val))) {
+                        finish(true);
+                    } else {
+                        finish(false);
+                    }
+                }
+            }
+        });
+    });
+});
+
+function finish(something) {
+    if (something === true) {
+        console.log("Succes");
+        return true;
+    } else {
+        console.log("Echec");
+        return false;
+    }
+}
---
+++ modules/digicode/digicode.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Old+South+Arabian&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./d_style.css">
+</head>
+
+<body>
+    <div class="grid-container">
+        <?php
+            for ($i = 0; $i < 9; $i++) {
+                echo "<button class=\"square\"></button>";
+            }
+        ?>
+    </div>
+    <script src="./d_scripts.js"></script>
+</body>
+
+</html>
