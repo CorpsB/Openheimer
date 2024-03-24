47944f1bc8f5104415457d2635e6e51973c4d7c8
Rudolf Tom <tom.rudolf@epitech.eu>
2024-03-24 18:13:23 +0100

    feat: add join page

--- game/join.php
+++ game/join.php
@@ -4,11 +4,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Open-heimer | join session</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap">
+    <link rel="stylesheet" href="../css/join.css">
+    <title>Open-Heimer | join game</title>
 </head>
 
 <body>
+    <header class="header">
+        <div class="header-infos">
+            <a href="/"><img src="../assets/openheimer_logo.png" alt="logo" href="/"></a>
+            <div class="header-text">
+                <h1>Open-Heimer</h1>
+                <h3>keep talking and everybody explodes</h3>
+            </div>
+        </div>
+    </header>
 
+    <div class="body">
+        <div class="container">
+            <form action="doc.php" method="POST"> <!-- Changed method to POST -->
+                <label for="idInput">Enter ID:</label>
+                <input type="text" id="idInput" name="id" required>
+                <button type="submit">Join</button>
+            </form>
+        </div>
+    </div>
 </body>
 
 </html>
