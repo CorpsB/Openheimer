32e49210345bdbede3ea6968622b941829cc7782
ncarabin <noe.carabin@epitech.eu>
2024-03-24 20:21:58 +0100

    Fix: Not change html size

--- modules/button_picto/b_p.php
+++ modules/button_picto/b_p.php
@@ -5,19 +5,19 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Old+South+Arabian&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="../modules/button_picto/b_p_style.css">
+    <link rel="stylesheet" href="./b_p_style.css">
 </head>
 
 <body>
     <?php
-    $characters = ['𐩺', '𐩱', '𐩵', '𐩷', '𐩢', '𐩣', '𐩨', '𐩤', '𐩥'];
-    echo '<div class="grid-container">';
-    foreach ($characters as $index => $char) {
-        echo "<button class=\"square\" data-char=\"{$char}\">{$char}</button>";
-    }
-    echo '</div>';
+        $characters = ['𐩺', '𐩱', '𐩵', '𐩷', '𐩢', '𐩣', '𐩨', '𐩤', '𐩥'];
+        echo '<div class="grid-container">';
+        foreach ($characters as $index => $char) {
+            echo "<button class=\"square\" data-char=\"{$char}\">{$char}</button>";
+        }
+        echo '</div>';
     ?>
-    <script src="../modules/button_picto/b_p_script.js"></script>
+    <script src="./b_p_script.js"></script>
 </body>
 
 </html>
