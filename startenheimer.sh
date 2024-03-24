ebab27e71c0848503132c1e280e59972ab3c11a1
Rudolf Tom <tom.rudolf@epitech.eu>
2024-03-24 18:57:44 +0100

    fix: de la merde

--- startenheimer.sh
+++ startenheimer.sh
@@ -1,16 +1,2 @@
 #!/bin/bash
-
-# Démarrer le serveur PHP sur localhost:4000
-php -S localhost:4000 > /dev/null 2>&1 &
-
-# Récupérer le PID du serveur PHP
-PHP_SERVER_PID=$!
-
-# Lancer le navigateur sur localhost:4000
-xdg-open http://localhost:4000
-
-# Attendre que l'utilisateur interrompe le script
-read -p "Appuyez sur Entrée pour arrêter le serveur..."
-
-# Arrêter le serveur PHP
-kill $PHP_SERVER_PID > /dev/null 2>&1
+php -S localhost:4000 & xdg-open http://localhost:4000
