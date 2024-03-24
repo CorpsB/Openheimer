46d9b6f924af0ecac2a84d60f5c802c02d7cce85
ncarabin <noe.carabin@epitech.eu>
2024-03-24 10:31:30 +0100

    Fix : Button/ Red button is center, text is un-selectable

--- modules/button/b_script.js
+++ modules/button/b_script.js
@@ -1,14 +1,14 @@
 const cercleRouge = document.getElementsByClassName("cercle-rouge")[0];
 
 cercleRouge.addEventListener('mousedown', function() {
-    this.style.backgroundColor = "blue";
+    cercleRouge.style.boxShadow = '0 0 5px 0 rgba(0, 0, 0, 0.5)';
 });
 cercleRouge.addEventListener('mouseup', function() {
-    this.style.backgroundColor = "red";
+    cercleRouge.style.boxShadow = '0 0 20px 0 rgba(0, 0, 0, 0.5)';
 });
-cercleRouge.addEventListener('mouseout', function() {
-    cercleRouge.style.boxShadow = '0 0 10px 0 rgba(0, 0, 0, 0.5)';
-});
+// cercleRouge.addEventListener('mouseout', function() {
+//     cercleRouge.style.boxShadow = '0 0 10px 0 rgba(0, 0, 0, 0.5)';
+// });
 cercleRouge.addEventListener('mouseover', function() {
-    cercleRouge.style.boxShadow = 'none';
+    cercleRouge.style.cursor = "pointer";
 });
